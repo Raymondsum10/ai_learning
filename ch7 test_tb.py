f0a8e6905d61948574d48b86e9264c173a9a26eb
Raymondsum10 <142603357+Raymondsum10@users.noreply.github.com>
2023-08-21 23:34:21 +0800

    Update ch7 test_tb.py

--- ch7 test_tb.py
+++ ch7 test_tb.py
@@ -1,7 +1,11 @@
 from torch.utils.tensorboard import SummaryWriter
-
+import numpy as np
 writer = SummaryWriter("logs")
 
-writer.add_scalar()
+
+for i in range(100):
+    writer.add_scalar("y=2x", 2*i, i)
 
 writer.close()
+
+
