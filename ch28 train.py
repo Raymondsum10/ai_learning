b9cbee97fcd366df7c5b0937b69202ccc8e90714
Raymondsum10 <142603357+Raymondsum10@users.noreply.github.com>
2023-09-04 21:01:33 +0800

    Update ch28 train.py

--- ch28 train.py
+++ ch28 train.py
@@ -6,18 +6,19 @@ from model import *
 from torch import nn
 from torch.utils.data import DataLoader
 
+#設置train的data，把他們變tensor
 train_data = torchvision.datasets.CIFAR10(root="../data", train=True, transform=torchvision.transforms.ToTensor(),
                                           download=True)
+#設置test的data，把他們變tensor
 test_data = torchvision.datasets.CIFAR10(root="../data", train=False, transform=torchvision.transforms.ToTensor(),
                                          download=True)
 
-# length 长度
+# 查看兩個dataset的length 長度
 train_data_size = len(train_data)
 test_data_size = len(test_data)
 # 如果train_data_size=10, 训练数据集的长度为：10
 print("训练数据集的长度为：{}".format(train_data_size))
 print("测试数据集的长度为：{}".format(test_data_size))
-
 
 # 利用 DataLoader 来加载数据集
 train_dataloader = DataLoader(train_data, batch_size=64)
@@ -44,13 +45,14 @@ total_test_step = 0
 epoch = 10
 
 # 添加tensorboard
+#查看的command : tensorboard --logdir="../logs_train" --port=6007
 writer = SummaryWriter("../logs_train")
 
 for i in range(epoch):
     print("-------第 {} 轮训练开始-------".format(i+1))
 
     # 训练步骤开始
-    tudui.train()
+    tudui.train()   #變成train mode,會對於特殊layer有用，例如dropout
     for data in train_dataloader:
         imgs, targets = data
         outputs = tudui(imgs)
@@ -67,10 +69,10 @@ for i in range(epoch):
             writer.add_scalar("train_loss", loss.item(), total_train_step)
 
     # 测试步骤开始
-    tudui.eval()
+    tudui.eval()    #把這個模型變成eval mode，對於特殊layer有用，例如dropout
     total_test_loss = 0
     total_accuracy = 0
-    with torch.no_grad():
+    with torch.no_grad():   #梯度的調整
         for data in test_dataloader:
             imgs, targets = data
             outputs = tudui(imgs)
@@ -85,6 +87,7 @@ for i in range(epoch):
     writer.add_scalar("test_accuracy", total_accuracy/test_data_size, total_test_step)
     total_test_step = total_test_step + 1
 
+    #保存模型>>完全模型+參數
     torch.save(tudui, "tudui_{}.pth".format(i))
     print("模型已保存")
 
